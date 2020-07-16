a24535f1bbd9d23aedc4b5abef5110c0448df682
Naruu <esara2021@gmail.com>
2020-07-16 00:12:27 +0000

    on week3

--- observatory/src/main/scala/observatory/Interaction.scala
+++ observatory/src/main/scala/observatory/Interaction.scala
@@ -14,14 +14,12 @@ object Interaction extends InteractionInterface {
     * @return The latitude and longitude of the top-left corner of the tile, as per http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
     */
   def tileLocation(tile: Tile): Location = {
-    /*
     val n = pow(2, tile.zoom)
     val lon_deg = tile.x / n * 360.0 - 180.0
     val lat_rad = atan(sinh(Pi * (1 - 2 * tile.y / n)))
     val lat_deg = lat_rad * 180.0 / Pi
     Location(lat_deg, lon_deg)
-    */
-    ???
+
   }
 
   /**
@@ -31,21 +29,18 @@ object Interaction extends InteractionInterface {
     * @return A 256×256 image showing the contents of the given tile
     */
   def tile(temperatures: Iterable[(Location, Temperature)], colors: Iterable[(Temperature, Color)], tile: Tile): Image = {
-    /*
       //val n = pow(2, tile.zoom+8).toInt
-      val coords = for(i <- 0 to 255; j <- 0 to 255) yield tileLocation(Tile(tile.x + i, tile.y + j , tile.zomm + 8))
+      val coords = for(i <- 0 to 255; j <- 0 to 255) yield (tile.y + i, tile.x + j)
 
-      val pixels = 
-      coords.par
-      .map(predictTemperature(temperatures, _))
-      .map(interpolateColor(colors, _))
-      .map({
-        case color:Color => Pixel(color.red, color.green, color.blue, 127)
-      })
+    val pixels = coords.par
+    .map({
+      case (y, x) =>
+      (y, x, interpolateColor(colors, predictTemperature(temperatures, tileLocation(Tile(y,x , tile.zoom + 8)))))
+    }).map({
+      case (x,y, color:Color) => (x, y, Pixel(color.red, color.green, color.blue, 127))
+    }).toArray.sortBy(x => (x._1, x._2)).map(_._3)
 
-      Image(256, 256, pixels.toArray)
-      */
-      ???
+    Image(256,256, pixels)
   }
 
   /**
